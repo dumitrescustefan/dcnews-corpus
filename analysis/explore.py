3106bf57ecb70e9f35786dc49820dea36d0f1105
andrei.pruteanu <andrei.pruteanu@gmail.com>
2021-10-17 10:46:18 +0300

    added separate script for running

--- analysis/explore.py
+++ analysis/explore.py
@@ -1,27 +1,10 @@
 import os
-import re
 import json
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from nltk.tokenize import word_tokenize
 from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
-
-# 1. Data Load
-print('Started loading data')
-if 'all.pkl' not in os.listdir('.'):
-    print('Did not find pickled data. Reading from JSON file')
-    with open('../all.json') as f:
-        all_data = json.loads(f.read())
-        all_data_df = pd.DataFrame.from_dict(all_data)
-        all_data_df.to_pickle("./all.pkl")
-else:
-    print('Found data already pickled.. reading that!')
-    all_data_df = pd.read_pickle('all.pkl')
-
-# all_data = {'title': ['title1', 'title2'], 'text': ['text1', 'text2']}
-
-print('Done loading data')
 
 
 # 2. TF-IDF + SVD + Cosine
@@ -45,41 +28,57 @@ def tokenize_split_join(raw_txt):
     return ' '.join(word_tokenize(raw_txt.replace('\n', ' ')))
 
 
-# combine title + text - this should be stored to disk to avoid computing it all the time
-print('Started creating corpuses')
-if 'corpus_both.pkl' not in os.listdir('.'):
-    print('Did not find pickled title+text pickle. Creating it..')
-    corpus_both_raw = all_data_df.title + ' ' + all_data_df.text
-    corpus_both = corpus_both_raw.apply(tokenize_split_join)
-    corpus_both.to_pickle('corpus_both.pkl')
-    print('Done pickling both preprocessed title+text')
-else:
-    print('Found pickled title+text pickle. Loading that!')
-    corpus_both = pd.read_pickle('corpus_both.pkl')
+def main(max_features=60000):
+    # 1. Data Load
+    print('Started loading data')
+    if 'all.pkl' not in os.listdir('.'):
+        print('Did not find pickled data. Reading from JSON file')
+        with open('../all.json') as f:
+            all_data = json.loads(f.read())
+            all_data_df = pd.DataFrame.from_dict(all_data)
+            all_data_df.to_pickle("./all.pkl")
+    else:
+        print('Found data already pickled.. reading that!')
+        all_data_df = pd.read_pickle('all.pkl')
 
-if 'corpus_title.pkl' not in os.listdir('.'):
-    corpus_title = all_data_df.title.apply(tokenize_split_join)
-    corpus_title.to_pickle('corpus_title.pkl')
-    print('Done pickling preprocessed title')
-else:
-    print('Found pickled title pickle. Loading that!')
-    corpus_title = pd.read_pickle('corpus_title.pkl')
+    # all_data = {'title': ['title1', 'title2'], 'text': ['text1', 'text2']}
 
-if 'corpus_text.pkl' not in os.listdir('.'):
-    corpus_text = all_data_df.text.apply(tokenize_split_join)
-    corpus_text.to_pickle('corpus_text.pkl')
-    print('Done pickling preprocessed text')
-else:
-    print('Found pickled text pickle. Loading that!')
-    corpus_text = pd.read_pickle('corpus_text.pkl')
+    print('Done loading data')
 
-print('Done creating all corpuses')
+    # combine title + text - this should be stored to disk to avoid computing it all the time
+    print('Started creating corpuses')
+    if 'corpus_both.pkl' not in os.listdir('.'):
+        print('Did not find pickled title+text pickle. Creating it..')
+        corpus_both_raw = all_data_df.title + ' ' + all_data_df.text
+        corpus_both = corpus_both_raw.apply(tokenize_split_join)
+        corpus_both.to_pickle('corpus_both.pkl')
+        print('Done pickling both preprocessed title+text')
+    else:
+        print('Found pickled title+text pickle. Loading that!')
+        corpus_both = pd.read_pickle('corpus_both.pkl')
 
-# create & fit model
-for max_features_item in range(160000, 220000, 20000):
-    print('Started fitting model for max_features {}'.format(max_features_item))
-    tfidf_model = fit_tfidf_model(list(corpus_both), max_features=max_features_item)
-    print('Done fitting the model for {} max_features'.format(max_features_item))
+    if 'corpus_title.pkl' not in os.listdir('.'):
+        corpus_title = all_data_df.title.apply(tokenize_split_join)
+        corpus_title.to_pickle('corpus_title.pkl')
+        print('Done pickling preprocessed title')
+    else:
+        print('Found pickled title pickle. Loading that!')
+        corpus_title = pd.read_pickle('corpus_title.pkl')
+
+    if 'corpus_text.pkl' not in os.listdir('.'):
+        corpus_text = all_data_df.text.apply(tokenize_split_join)
+        corpus_text.to_pickle('corpus_text.pkl')
+        print('Done pickling preprocessed text')
+    else:
+        print('Found pickled text pickle. Loading that!')
+        corpus_text = pd.read_pickle('corpus_text.pkl')
+
+    print('Done creating all corpuses')
+
+    # create & fit model
+    print('Started fitting model for max_features {}'.format(max_features))
+    tfidf_model = fit_tfidf_model(list(corpus_both), max_features=max_features)
+    print('Done fitting the model for {} max_features'.format(max_features))
 
     # create reduced vectors
     print('Started creating reduced vectors')
@@ -88,7 +87,7 @@ for max_features_item in range(160000, 220000, 20000):
     print('Done creating reduced vectors')
 
     # compute similarity score
-    print('Started computing similarity score')
+    print('Started computing similarity score for {} max_features'.format(max_features))
     all_data_df['sim_score'] = 0.0
     for cnt, (idx, row) in enumerate(all_data_df.iterrows()):
         sim_score = cosine_similarity([reduced_vectors_titles[cnt]], [reduced_vectors_texts[cnt]])
@@ -97,6 +96,19 @@ for max_features_item in range(160000, 220000, 20000):
             print('Processed {} title/text pairs'.format(cnt))
     print('Done computing similarity score')
 
-    all_data_df.to_pickle("./all_data_sim_score_max_features_{}.pkl".format(max_features_item))
+    all_data_df.to_pickle("./all_data_sim_score_max_features_{}.pkl".format(max_features))
 
-print('That\'s all folks!')
+    print('Done computing similarity score for {} max_features'.format(max_features))
+
+
+def read_cmd_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--max-features', metavar='mf', type=int,
+                        help='an integer for the max # of features')
+    ret = parser.parse_args()
+    return ret
+
+
+args = read_cmd_args()
+main(args.max_features)
---
+++ analysis/run.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+
+def main():
+    for max_features_item in range(160000, 220000, 20000):
+        os.system('python explore.py --max-features {}'.format(max_features_item))
+
+        # process = subprocess.Popen(['python', 'explore.py', '--max-features', str(max_features_item)],
+        #                            stdout=subprocess.PIPE,
+        #                            universal_newlines=True)
+        #
+        # while True:
+        #     output = process.stdout.readline()
+        #     print(output.strip())
+        #     # Do something else
+        #     return_code = process.poll()
+        #     if return_code is not None:
+        #         print('RETURN CODE', return_code)
+        #         # Process has finished, read rest of the output
+        #         for output in process.stdout.readlines():
+        #             print(output.strip())
+        #         break
+
+
+main()
